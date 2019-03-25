45ca5bb2ecceb5e0ec83c917d5e71d4e00915c18
Kevin Robinson <kevinrobinsondeveloper@gmail.com>
2019-03-25 11:14:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+//submit handler
+let inputBreed = ""
+
+
+function getDogImage() {
+  let fetchAddress = `https://dog.ceo/api/breed/${inputBreed}/images/random`
+  fetch(fetchAddress)
+    .then(response => response.json())
+    .then(responseJson =>     
+      displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'));
+}
+
+function displayResults(responseJson) {
+    console.log(responseJson);
+    //replace the existing image with the new one
+      $('.results-img').append(
+        `<img src="${responseJson.message}" class="results-img">`
+      )
+
+    //display the results section
+    $('.results').removeClass('hidden');
+    console.log('done??!?!')
+  }
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    inputBreed = document.getElementById("thing").value;
+    console.log(inputBreed);
+    //$('.results').empty();
+    $('.results-img').empty();
+    getDogImage();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+
+});
+
